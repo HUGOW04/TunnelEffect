19b925f960d042a8657e3f17c2c918e53bbd8788
Hugo <54809176+HUGOW04@users.noreply.github.com>
2022-07-07 12:25:34 +0200

    done

--- TunnelEffect/main.c
+++ TunnelEffect/main.c
@@ -5,7 +5,7 @@ int main()
 {
 	FreeConsole();
 	int a, b;
-	while (true) {
+	while (1) {
 		a = GetSystemMetrics(SM_CXSCREEN);
 		b = GetSystemMetrics(SM_CYSCREEN);
 		StretchBlt(GetDC(NULL), 50, 50, a - 100, b - 100, GetDC(NULL), 0, 0, a, b, SRCCOPY);
